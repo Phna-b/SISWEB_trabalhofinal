a4e4f58eb633542dcf8a82a33fc9c818e5a6042c
Pedro Henrique Nunes de Assis <phna8589@gmail.com>
2025-01-27 12:53:39 -0300

    Atualização

--- ProjFinal/app.py
+++ ProjFinal/app.py
@@ -19,11 +19,27 @@ class Pessoa(db.Model):
     cpf = db.Column(db.String)
     email = db.Column(db.String,unique=True)
 
-    def __init__(self, nome, telefone, cpf, email):
+
+    def __init__(self, nome, telefone, cpf, email,password):
         self.nome = nome
         self.telefone = telefone
         self.cpf = cpf
         self.email = email
+
+
+class Conta(db.Model):
+
+    __tablename__ = 'conta'
+
+    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String)
+    email = db.Column(db.String,unique=True)
+    password = db.Column(db.String(100))
+
+    def __init__(self, nome, email,password):
+        self.nome = nome
+        self.email = email
+        self.password = password
 
 db.create_all()
 
@@ -82,9 +98,44 @@ def atualizar(id):
             return redirect(url_for("lista"))
     return render_template("atualizar.html",pessoa=pessoa)
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    nome = request.form.get('nome')
+    password = request.form.get('password')
+
+
+    user = Conta.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        return redirect(url_for('signup'))
+
+    if nome and email and password:
+        new_user = Conta(email=email, nome=nome,password=generate_password_hash(password, method='pbkdf2:sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+
+
+
+
+
+
+
+
+
 @app.route("/")
 def base():
-    return render_template("login.html")
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
